ca717352c51b34ce3121a624b4c02bfd7539a7cf
Hamid Elaosta <hamid@2byt.es>
2016-06-26 18:12:56 +0100

    Added initial tests thanks to comments from #golang community on Twitter. Tweaked dodns to accomodate tests, and to fix issues pointed out by the tests ;)

--- dodns.go
+++ dodns.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"flag"
-	"net/http"
-	"net"
-	"io/ioutil"
-	"strings"
-	"encoding/json"
 	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
 const version string = "0.1"
@@ -22,8 +23,8 @@ const version string = "0.1"
 		echo $_SERVER["REMOTE_ADDR"]."\n";
 	?>
 	===================== SNIP ====================
- */
-var ip_check_addresses []string = []string {
+*/
+var ip_check_addresses []string = []string{
 	"http://ipecho.net/plain",
 	"http://ipinfo.io/ip",
 }
@@ -32,18 +33,18 @@ const do_api_root string = "https://api.digitalocean.com/v2"
 const do_api_records string = do_api_root + "/domains/%s/records"
 const do_api_record string = do_api_records + "/%d"
 
-var binName* string
+var binName *string
 
-var ipFlag* string
+var ipFlag *string
 
 type domain_record struct {
-	ID int `json:"id"`
-	Type string `json:"type"`
-	Name string `json:"name"`
-	Data string `json:"data"`
-	Priority int `json:"priority"`
-	Port string `json:"port"`
-	Weight string `json:"weight"`
+	ID       int    `json:"id"`
+	Type     string `json:"type"`
+	Name     string `json:"name"`
+	Data     string `json:"data"`
+	Priority int    `json:"priority"`
+	Port     string `json:"port"`
+	Weight   string `json:"weight"`
 }
 
 type a_domain_record struct {
@@ -93,8 +94,14 @@ func decode_ip_check_response(response *http.Response) (error, net.IP) {
 	return fmt.Errorf("Unacceptable response code: %q", response.StatusCode), nil
 }
 
-func get_ip_from(url string) (error, net.IP) {
-	resp,err := http.Get(url)
+func get_ip_from(urlString string) (error, net.IP) {
+
+	url, err := url.Parse(urlString)
+	if err != nil {
+		return fmt.Errorf("Provided url string [ %s ] can not be parsed: %v", urlString, err), nil
+	}
+
+	resp, err := http.Get(url.String())
 
 	if err != nil {
 		return fmt.Errorf("Check IP (GET) failed: %q", err), nil
@@ -117,20 +124,20 @@ func get_ip_from(url string) (error, net.IP) {
 
 func check_my_ip() (error, string) {
 
-	for _,remote := range ip_check_addresses {
+	for _, remote := range ip_check_addresses {
 		err, ip := get_ip_from(remote)
 
 		if err == nil {
 			return nil, ip.String()
 		} else {
-			fmt.Println(err.Error())
+			return err, ""
 		}
 	}
 
 	return fmt.Errorf("Failed to retrieve IP from all (%d) remotes\n", len(ip_check_addresses)), ""
 }
 
-func update_record_with_ip(ip_addr string, token_string string, domain_name string, record domain_record) (error){
+func update_record_with_ip(ip_addr string, token_string string, domain_name string, record domain_record) error {
 
 	dr := new(a_domain_record)
 
@@ -142,7 +149,7 @@ func update_record_with_ip(ip_addr string, token_string string, domain_name stri
 	json.NewEncoder(b).Encode(record)
 
 	req, err := http.NewRequest("PUT", reqString, b)
-	req.Header.Add("Authorization", "Bearer " + token_string)
+	req.Header.Add("Authorization", "Bearer "+token_string)
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := client.Do(req)
 
@@ -179,12 +186,12 @@ func update_record_with_ip(ip_addr string, token_string string, domain_name stri
 
 func has_ip_changed(ip_addr string, token_string string, domain_name string, record_name string) (error, domain_record, bool) {
 
-	dr := new(domain_records)
+	drs := new(domain_records)
 
 	client := &http.Client{}
 	reqString := fmt.Sprintf(do_api_records, domain_name)
 	req, err := http.NewRequest("GET", reqString, nil)
-	req.Header.Add("Authorization", "Bearer " + token_string)
+	req.Header.Add("Authorization", "Bearer "+token_string)
 	resp, err := client.Do(req)
 
 	if err != nil {
@@ -201,28 +208,80 @@ func has_ip_changed(ip_addr string, token_string string, domain_name string, rec
 		print_error(fmt.Errorf("Non-OK status code returned [ %d ]; %q\n", resp.StatusCode, resp.Status).Error())
 	}
 
-	err = json.NewDecoder(resp.Body).Decode(dr)
+	err = json.NewDecoder(resp.Body).Decode(drs)
 
 	if err != nil {
 		return fmt.Errorf("Decoding JSON response failed: %q", err), domain_record{}, false
 	}
 
-	if dr.Records == nil {
+	if drs.Records == nil {
 		return fmt.Errorf("Record not found: [ %q ]", record_name), domain_record{}, false
 	}
 
-	for _,record := range dr.Records {
-		if(record.Name == record_name) {
+	for _, record := range drs.Records {
+		if record.Name == record_name {
 			if ip_addr == record.Data {
 				return nil, record, false
 			} else {
-				return nil ,record, true
+				return nil, record, true
 			}
 		}
 	}
 
 	return fmt.Errorf("Record not found [ %s ] in domain [ %s ]. Add it in the DO control panel first.", record_name, domain_name), domain_record{}, false
 
+}
+
+func check_domain_valid(dom string) error {
+	add, err := url.Parse(dom)
+
+	if err != nil {
+		return fmt.Errorf("Failed to parse domain [ %v ]", dom)
+	}
+
+	if add.Scheme != "" {
+		return fmt.Errorf("Scheme should not be specified, you gave [ %v ]", add.Scheme)
+	}
+
+	addrs, err := net.LookupHost(dom)
+
+	if err != nil {
+		return fmt.Errorf("Failed to lookup host [ %v ], error : %v", add.Host, err)
+	}
+
+	if len(addrs) < 1 {
+		return fmt.Errorf("No valid addresses found for domain [ %v ]", add.String())
+	}
+
+	return nil
+}
+
+func check_record_valid(record string, domain string) error {
+
+	err := check_domain_valid(domain)
+	if err != nil {
+		return fmt.Errorf("Domain invalid, cannot test record")
+	}
+
+	fullHost := record + "." + domain
+
+	add, err := url.Parse(fullHost)
+
+	if err != nil {
+		return fmt.Errorf("Failed to parse record [ %v ] with domain [ %v ]", record, domain)
+	}
+
+	addrs, err := net.LookupHost(fullHost)
+
+	if err != nil {
+		return fmt.Errorf("Failed to lookup host [ %v ], error : %v", add.Host, err)
+	}
+
+	if len(addrs) < 1 {
+		return fmt.Errorf("No valid addresses found for domain [ %v ]", add.String())
+	}
+
+	return nil
 }
 
 func init() {
@@ -251,6 +310,20 @@ func main() {
 		os.Exit(1)
 	}
 
+	err := check_domain_valid(domain)
+
+	if err != nil {
+		print_error(err.Error())
+		os.Exit(2)
+	}
+
+	err = check_record_valid(record, domain)
+
+	if err != nil {
+		print_error(err.Error())
+		os.Exit(3)
+	}
+
 	var ip string
 
 	if *ipFlag != "" {
@@ -261,6 +334,7 @@ func main() {
 		err, ip_ext := check_my_ip()
 		if err != nil {
 			print_error(fmt.Errorf("External lookup failed: %q\n", err).Error())
+			os.Exit(4)
 		} else {
 			ip = ip_ext
 		}
@@ -270,7 +344,7 @@ func main() {
 
 	if err != nil {
 		print_error(fmt.Errorf("DNS update fail: %s\n", err.Error()).Error())
-		os.Exit(2)
+		os.Exit(5)
 	}
 
 	if changed {
@@ -288,5 +362,5 @@ func main() {
 	}
 
 	print_error("Unknown error. Please report this.")
-	os.Exit(3)
+	os.Exit(6)
 }
---
+++ dodns_test.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"net/url"
+	"os"
+	"testing"
+)
+
+const (
+	validRecord    string = "1"
+	invalidRecord  string = "-1"
+	validDomain    string = "2byt.es"
+	invalidDomain  string = "2;byt.es"
+	includesScheme string = "https://2byt.es"
+)
+
+var (
+	// https://developers.digitalocean.com/documentation/v2/#domain-records
+	validRecordJSON []byte = []byte(`
+	{
+		"id": 3352895,
+		"type": "A",
+		"name": "@",
+		"data": "1.2.3.4",
+		"priority": null,
+		"port": null,
+		"weight": null
+	}
+	`)
+
+	invalidRecordJSON []byte = []byte(`
+	{
+		"id": 3352895,
+		"name": "@",
+		"data": "1.2.3.4",
+		"priority": low,
+		"port": null,
+		"weight": null
+	}
+	`)
+)
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	os.Exit(m.Run())
+}
+
+func TestURLCheckIPServiceConstants(t *testing.T) {
+
+	for _, dom := range ip_check_addresses {
+		_, err := url.Parse(dom)
+		if err != nil {
+			t.Errorf("IP check site url [ %s ] could not be parsed/is invalid. Error: %v", dom, err.Error())
+		}
+	}
+}
+
+func TestURLInvalidCheckIPService(t *testing.T) {
+
+	err, _ := get_ip_from("get:ip;from.invalid_site.com")
+
+	if err == nil {
+		t.Error("An invalid IP search service passed parsing")
+	}
+}
+
+func TestParseInputValidDomain(t *testing.T) {
+	err := check_domain_valid(validDomain)
+	if err != nil {
+		t.Errorf("Testing valid input domain failed: %v", err.Error())
+	}
+}
+
+func TestParseInputInvalidDomain(t *testing.T) {
+	err := check_domain_valid(invalidDomain)
+
+	if err == nil {
+		t.Error("An invalid input domain passed parsing")
+	}
+}
+
+func TestParseInputInvalidIncludesScheme(t *testing.T) {
+	err := check_domain_valid(includesScheme)
+
+	if err == nil {
+		t.Error("An invalid input domain (with scheme) passed parsing")
+	}
+}
+
+func TestParseInputValidRecordValidDomain(t *testing.T) {
+	err := check_record_valid(validRecord, validDomain)
+
+	if err != nil {
+		t.Errorf("Testing valid input record failed: %v", err.Error())
+	}
+}
+
+func TestParseInputInvalidRecordValidDomain(t *testing.T) {
+	err := check_record_valid(invalidRecord, validDomain)
+
+	if err == nil {
+		t.Error("An invalid record with a valid domain passed parsing")
+	}
+}
+
+func TestParseInputValidRecordInvalidDomain(t *testing.T) {
+	err := check_record_valid(validRecord, invalidDomain)
+
+	if err == nil {
+		t.Error("A valid record with invalid domain test passed parsing")
+	}
+}
+
+func TestParseInputInvalidRecordInvalidDomain(t *testing.T) {
+	err := check_record_valid(invalidRecord, invalidDomain)
+
+	if err == nil {
+		t.Error("An invalid record with an invalid domain test passed parsing")
+	}
+}
+
+func TestParseValidDomainRecordJSON(t *testing.T) {
+	dr := new(domain_record)
+
+	err := json.Unmarshal(validRecordJSON, dr)
+	if err != nil {
+		t.Error("Failed to unmarshal valid JSON record; %v", err)
+	}
+}
+
+func TestParseInvalidDomainRecordJSON(t *testing.T) {
+	dr := new(domain_record)
+
+	err := json.Unmarshal(invalidRecordJSON, dr)
+	if err == nil {
+		t.Error("An invalid domain record reponse JSON passed parsing")
+	}
+}
